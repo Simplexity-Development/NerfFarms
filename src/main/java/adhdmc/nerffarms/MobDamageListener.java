0f8fcc4ef0bbbe88c56636dfa1446ef61501c026
Peashooter101 <Peashooter101@yahoo.com>
2022-08-12 14:40:32 -0700

    Pushing for testing nerfing.
    Removed most checks except for if it is a Mob. If it is a mob, it will be nerfed on a killing blow.
    
    Testing for the functionality of the nerfing PDC on the mob.
    
    Nerfing will follow the Modification setting in the Configuration.

--- src/main/java/adhdmc/nerffarms/MobDamageListener.java
+++ src/main/java/adhdmc/nerffarms/MobDamageListener.java
@@ -41,87 +41,88 @@ public class MobDamageListener implements Listener {
             }
             return;
         }
-        if (ConfigParser.isNerfHostilesOnly() && !(damagedEntity instanceof Monster)) {
-            if (d) {
-                l.info("Ignoring onMobDamage because " + damagedEntity.getName() + " is not a Monster and Nerf Hostiles Only is True.");
-            }
-            return;
-        }
-        if (!ConfigParser.getSpawnReasonList().contains(damagedEntity.getEntitySpawnReason())) {
-            if (d) {
-                l.info("Ignoring onMobDamage because " + damagedEntity.getName() + " spawned from " + damagedEntity.getEntitySpawnReason() + " which isn't nerfed.");
-            }
-            return;
-        }
-        if (ConfigParser.getBypassList().contains(damagedEntity.getType())) {
-            if (d) {
-                l.info("Ignoring onMobDamage because " + damagedEntity.getName() + " is on the bypass list as " + damagedEntity.getType());
-            }
-            return;
-        }
+//        if (ConfigParser.isNerfHostilesOnly() && !(damagedEntity instanceof Monster)) {
+//            if (d) {
+//                l.info("Ignoring onMobDamage because " + damagedEntity.getName() + " is not a Monster and Nerf Hostiles Only is True.");
+//            }
+//            return;
+//        }
+//        if (!ConfigParser.getSpawnReasonList().contains(damagedEntity.getEntitySpawnReason())) {
+//            if (d) {
+//                l.info("Ignoring onMobDamage because " + damagedEntity.getName() + " spawned from " + damagedEntity.getEntitySpawnReason() + " which isn't nerfed.");
+//            }
+//            return;
+//        }
+//        if (ConfigParser.getBypassList().contains(damagedEntity.getType())) {
+//            if (d) {
+//                l.info("Ignoring onMobDamage because " + damagedEntity.getName() + " is on the bypass list as " + damagedEntity.getType());
+//            }
+//            return;
+//        }
         double entityHealth = ((Mob) damagedEntity).getHealth();
         double hitDamage = damageEvent.getFinalDamage();
-        int percentFromEnvironment = ConfigParser.getPercentFromEnvironment();
-        if (ConfigParser.getEnvironmentalDamageSet().contains(damageEvent.getCause())) {
-            if (d) {
-                l.info("Noting environmental damage of " + hitDamage + " to " + damagedEntity.getName() + ".");
-            }
-            addPDCDamage(mobPDC, hitDamage);
-        }
-        double envDamage = mobPDC.getOrDefault(environmentalDamage, PersistentDataType.DOUBLE, 0.0);
-        double maxHealth = Objects.requireNonNull(((Mob) damagedEntity).getAttribute(Attribute.GENERIC_MAX_HEALTH)).getValue();
-        int percentDamage = (int) ((envDamage / maxHealth) * 100);
-        if (percentDamage >= percentFromEnvironment) {
-            if (d) {
-                l.info("Nerfing " + damagedEntity.getName() + " because they took " + percentDamage + "% total damage from the environment.");
-            }
-            mobPDC.set(nerfMob, PersistentDataType.BYTE, t);
-            return;
-        }
+//        int percentFromEnvironment = ConfigParser.getPercentFromEnvironment();
+//        if (ConfigParser.getEnvironmentalDamageSet().contains(damageEvent.getCause())) {
+//            if (d) {
+//                l.info("Noting environmental damage of " + hitDamage + " to " + damagedEntity.getName() + ".");
+//            }
+//            addPDCDamage(mobPDC, hitDamage);
+//        }
+//        double envDamage = mobPDC.getOrDefault(environmentalDamage, PersistentDataType.DOUBLE, 0.0);
+//        double maxHealth = Objects.requireNonNull(((Mob) damagedEntity).getAttribute(Attribute.GENERIC_MAX_HEALTH)).getValue();
+//        int percentDamage = (int) ((envDamage / maxHealth) * 100);
+//        if (percentDamage >= percentFromEnvironment) {
+//            if (d) {
+//                l.info("Nerfing " + damagedEntity.getName() + " because they took " + percentDamage + "% total damage from the environment.");
+//            }
+//            mobPDC.set(nerfMob, PersistentDataType.BYTE, t);
+//            return;
+//        }
         if (!(entityHealth - hitDamage <= 0)) {
             l.info("Ignoring onMobDamage because " + damagedEntity.getName() + " is not dying.");
             return;
         }
-        Location mobLocation = damagedEntity.getLocation();
-        Location mobStandingOnLocation = damagedEntity.getLocation().subtract(0, 1, 0);
-        Material entityStandingOn = mobStandingOnLocation.getBlock().getType();
-        Material entityStandingIn = mobLocation.getBlock().getType();
-        EntityDamageEvent.DamageCause damageType = damageEvent.getCause();
-        Entity entityDamager = damageEvent.getDamager();
-        if (!ConfigParser.getDamageCauseWhitelist().contains(damageType)) {
-            if (d) {
-                l.info("Nerfing " + damagedEntity.getName() + " due to " + damageType);
-            }
-            mobPDC.set(nerfMob, PersistentDataType.BYTE, t);
-            return;
-        }
-        if (!(entityDamager instanceof Player)) {
-            if (d) {
-                l.info("Nerfing " + damagedEntity.getName() + " because killer is not a player");
-            }
-            mobPDC.set(nerfMob, PersistentDataType.BYTE, t);
-            return;
-        }
-        if (!hasPathToPlayer((Player) entityDamager, (Mob) damagedEntity)) {
-            if (d) {
-                l.info("Nerfing " + damagedEntity.getName() + " because they never could reach the player.");
-            }
-            mobPDC.set(nerfMob, PersistentDataType.BYTE, t);
-            return;
-        }
-        if (ConfigParser.getStandOnBlackList().contains(entityStandingOn)) {
-            if (d) {
-                l.info("Nerfing " + damagedEntity.getName() + " since they are standing on " + entityStandingOn);
-            }
-            mobPDC.set(nerfMob, PersistentDataType.BYTE, t);
-            return;
-        }
-        if (ConfigParser.getInsideBlackList().contains(entityStandingIn)) {
-            if (d) {
-                l.info("Nerfing " + damagedEntity.getName() + " since they are standing in " + entityStandingIn);
-            }
-            mobPDC.set(nerfMob, PersistentDataType.BYTE, t);
-        }
+        mobPDC.set(nerfMob, PersistentDataType.BYTE, t);
+//        Location mobLocation = damagedEntity.getLocation();
+//        Location mobStandingOnLocation = damagedEntity.getLocation().subtract(0, 1, 0);
+//        Material entityStandingOn = mobStandingOnLocation.getBlock().getType();
+//        Material entityStandingIn = mobLocation.getBlock().getType();
+//        EntityDamageEvent.DamageCause damageType = damageEvent.getCause();
+//        Entity entityDamager = damageEvent.getDamager();
+//        if (!ConfigParser.getDamageCauseWhitelist().contains(damageType)) {
+//            if (d) {
+//                l.info("Nerfing " + damagedEntity.getName() + " due to " + damageType);
+//            }
+//            mobPDC.set(nerfMob, PersistentDataType.BYTE, t);
+//            return;
+//        }
+//        if (!(entityDamager instanceof Player)) {
+//            if (d) {
+//                l.info("Nerfing " + damagedEntity.getName() + " because killer is not a player");
+//            }
+//            mobPDC.set(nerfMob, PersistentDataType.BYTE, t);
+//            return;
+//        }
+//        if (!hasPathToPlayer((Player) entityDamager, (Mob) damagedEntity)) {
+//            if (d) {
+//                l.info("Nerfing " + damagedEntity.getName() + " because they never could reach the player.");
+//            }
+//            mobPDC.set(nerfMob, PersistentDataType.BYTE, t);
+//            return;
+//        }
+//        if (ConfigParser.getStandOnBlackList().contains(entityStandingOn)) {
+//            if (d) {
+//                l.info("Nerfing " + damagedEntity.getName() + " since they are standing on " + entityStandingOn);
+//            }
+//            mobPDC.set(nerfMob, PersistentDataType.BYTE, t);
+//            return;
+//        }
+//        if (ConfigParser.getInsideBlackList().contains(entityStandingIn)) {
+//            if (d) {
+//                l.info("Nerfing " + damagedEntity.getName() + " since they are standing in " + entityStandingIn);
+//            }
+//            mobPDC.set(nerfMob, PersistentDataType.BYTE, t);
+//        }
     }
 
     private boolean hasPathToPlayer(Player p, Mob m) {
